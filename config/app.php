8db94317a207ea6516d101bb59b7f1a81d824e90
unknown <abdullayevilxomjon1@gmail.com>
2021-01-29 10:49:22 +0500

    Test

--- config/app.php
+++ config/app.php
@@ -195,7 +195,7 @@ return [
 
   'aliases' => [
 
-    'App' => Illuminate\Support\Facadesapp()->class,
+    'App' => Illuminate\Support\Facades\App::class,
     'Artisan' => Illuminate\Support\Facades\Artisan::class,
     'Auth' => Illuminate\Support\Facades\Auth::class,
     'Blade' => Illuminate\Support\Facades\Blade::class,
